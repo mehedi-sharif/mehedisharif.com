222225aa8ee9b56c43379ce41a2852dde6173531
Al Murad Uzzaman <murad.themefisher@gmail.com>
2024-06-29 15:52:40 +0600

    Email Validation pg added

--- src/layouts/helpers/TimelineEvents.jsx
+++ src/layouts/helpers/TimelineEvents.jsx
@@ -246,10 +246,11 @@ const TimelineEvents = ({ secretKey }) => {
                     </button>
                   </ul>
                   <div
-                    className={`fixed top-0 left-0 z-[99999] h-screen w-screen backdrop-blur backdrop-brightness-75 ${addEventForm || editEvent
-                      ? "visible opacity-100"
-                      : "invisible opacity-0"
-                      }`}
+                    className={`fixed top-0 left-0 z-[99999] h-screen w-screen backdrop-blur backdrop-brightness-75 ${
+                      addEventForm || editEvent
+                        ? "visible opacity-100"
+                        : "invisible opacity-0"
+                    }`}
                   >
                     <span
                       className="absolute top-0 left-0 -z-[1] h-full w-full"
@@ -285,10 +286,11 @@ const TimelineEvents = ({ secretKey }) => {
                             ) : (
                               <div>
                                 <div
-                                  className={`mb-4 flex ${editEvent
-                                    ? "justify-between"
-                                    : "justify-end"
-                                    } `}
+                                  className={`mb-4 flex ${
+                                    editEvent
+                                      ? "justify-between"
+                                      : "justify-end"
+                                  } `}
                                 >
                                   {editEvent && (
                                     <button
@@ -331,13 +333,13 @@ const TimelineEvents = ({ secretKey }) => {
                                     onChange={(e) =>
                                       addEventForm
                                         ? setNewEvent({
-                                          ...newEvent,
-                                          date: e.target.value,
-                                        })
+                                            ...newEvent,
+                                            date: e.target.value,
+                                          })
                                         : setEditEvent({
-                                          ...editEvent,
-                                          date: e.target.value,
-                                        })
+                                            ...editEvent,
+                                            date: e.target.value,
+                                          })
                                     }
                                   />
                                   <input
@@ -353,13 +355,13 @@ const TimelineEvents = ({ secretKey }) => {
                                     onChange={(e) =>
                                       addEventForm
                                         ? setNewEvent({
-                                          ...newEvent,
-                                          heading: e.target.value,
-                                        })
+                                            ...newEvent,
+                                            heading: e.target.value,
+                                          })
                                         : setEditEvent({
-                                          ...editEvent,
-                                          heading: e.target.value,
-                                        })
+                                            ...editEvent,
+                                            heading: e.target.value,
+                                          })
                                     }
                                   />
                                   <textarea
@@ -375,13 +377,13 @@ const TimelineEvents = ({ secretKey }) => {
                                     onChange={(e) =>
                                       addEventForm
                                         ? setNewEvent({
-                                          ...newEvent,
-                                          description: e.target.value,
-                                        })
+                                            ...newEvent,
+                                            description: e.target.value,
+                                          })
                                         : setEditEvent({
-                                          ...editEvent,
-                                          description: e.target.value,
-                                        })
+                                            ...editEvent,
+                                            description: e.target.value,
+                                          })
                                     }
                                   />
                                   <ImageUploader
@@ -417,7 +419,9 @@ const TimelineEvents = ({ secretKey }) => {
         </div>
       )}
       {modalImage && (
-        <div className={`fixed inset-0 z-[100000] flex items-center justify-center bg-black bg-opacity-75 ${modalImage ? 'animate-fade-in' : 'animate-fade-out'}`}>
+        <div
+          className={`fixed inset-0 z-[100000] flex items-center justify-center bg-black bg-opacity-75 ${modalImage ? "animate-fade-in" : "animate-fade-out"}`}
+        >
           <button
             className="absolute top-4 right-4 text-white"
             onClick={() => setModalImage(null)}
@@ -427,7 +431,7 @@ const TimelineEvents = ({ secretKey }) => {
           <img
             src={modalImage}
             alt="Full Size"
-            className={`max-w-full max-h-full rounded ${modalImage ? 'animate-fade-in' : 'animate-fade-out'}`}
+            className={`max-w-full max-h-full rounded ${modalImage ? "animate-fade-in" : "animate-fade-out"}`}
           />
         </div>
       )}
---
+++ src/pages/api/dailyAPICount.ts
@@ -0,0 +1,91 @@
+import type { APIRoute } from "astro";
+import { MongoClient, type MongoClientOptions } from "mongodb";
+
+const uri: string = import.meta.env.MONGODB_URI as string;
+const options: MongoClientOptions = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!import.meta.env.MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env");
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+const database = "mehedisharif";
+const collection = "dailyAPICount";
+
+export const GET: APIRoute = async () => {
+  try {
+    const client = await clientPromise;
+    const db = client.db(database);
+    const dailyAPICountData = await db.collection(collection).findOne({});
+    return new Response(JSON.stringify(dailyAPICountData?.count || 0), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("GET request failed:", error);
+    return new Response(
+      JSON.stringify({ error: "Failed to fetch verification count" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
+};
+
+export const POST: APIRoute = async () => {
+  try {
+    const client = await clientPromise;
+    const db = client.db(database);
+
+    const today = new Date().toISOString().split("T")[0];
+    const existingDocument = await db
+      .collection(collection)
+      .findOne({ date: today });
+
+    if (existingDocument) {
+      const result = await db
+        .collection(collection)
+        .updateOne({ date: today }, { $inc: { count: 1 } });
+      if (result.modifiedCount === 0) {
+        throw new Error("Failed to update daily count");
+      }
+    } else {
+      await db.collection(collection).insertOne({ date: today, count: 1 });
+    }
+
+    return new Response(JSON.stringify({ message: "Daily count updated" }), {
+      status: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("POST request failed:", error);
+    return new Response(
+      JSON.stringify({ error: "Failed to update daily count" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
+};
